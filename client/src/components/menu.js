0e92acc0129c8d62e195bec39d984de6f43084bc
Leonard003tac <alvaradoaguilaleonardoalberto@gmail.com>
2024-05-11 18:20:13 -0700

    MENU AGREGACION

--- client/src/components/menu.js
+++ client/src/components/menu.js
@@ -4,28 +4,66 @@ import { Menubar } from 'primereact/menubar';
 const Menu = () => {
     const items = [
         {
-            label: 'Sistema Prestamo',
+            label: 'Sistema de Inventario',
         },
-        {
-            label: 'Prestamo de Equipo',
-            icon: 'pi pi-building-columns',
-            //command: () => {window.location.href='/UnidadAcademica';}
-        }, 
-        
-        {
-            label: 'Historial de Prestamo',
-            icon: 'pi pi-briefcase',
-            //command: () => {window.location.href='/ProgramaEducativo';}
-        },
-
         {
             label: 'Inventario',
+            icon: 'pi pi-building',
+            items: [
+                {
+                    label: 'Inventario',
+                    command: () => {window.location.href='/inventario';}                 
+                },                
+                {
+                    label: 'Ubicacion de Inventario',
+                    command: () => {window.location.href='/ubicacioninventario';}                    
+                },
+            ]
+        },
+
+        {
+            label: 'Material',
             icon: 'pi pi-building-columns',
-            //command: () => {window.location.href='/UnidadAprendizaje';}
-        },                          
+            items: [
+                {
+                    label: 'Material',
+                    command: () => {window.location.href='/Material';}                 
+                },                
+                {
+                    label: 'Ubicacion de Material',
+                    command: () => {window.location.href='/UbicacionMaterial';}                    
+                },
+            ]
+        }, 
+
+        {
+            label: 'Categoria',
+            icon: 'pi pi-home',
+            command: () => {window.location.href='/Categoria';}
+
+        },
+        {
+            label: 'Estado',
+            icon: 'pi pi-home',
+            command: () => {window.location.href='/Estado';}
+        },                    
+
+        {
+            label: 'Modelo',
+            icon: 'pi pi-home',
+            command: () => {window.location.href='/Modelo';}
+        },                    
+
+         
+        {
+            label: 'Marca',
+            icon: 'pi pi-building-columns',
+            command: () => {window.location.href='/Marca';}
+        } 
+                         
 
     ];
-    const start = <img alt="logo" src="https://comunicacioninstitucional.uabc.mx/sites/default/files/inline-images/escudo-actualizado-2022.png" height="40" className="mr-2"></img>;         
+    const start = <img alt="logo" src="/images/image.png" height="40" className="mr-2"></img>;         
   return (
     <>
         <Menubar model={items} start={start}/>                              
