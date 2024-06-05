71066584ad32b19f72225f801749d27352faf39f
Joapava <josue.aparicio@uabc.edu.mx>
2024-06-05 16:58:17 -0700

    update

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -6,9 +6,46 @@ import { useState } from "react";
 import { Password } from 'primereact/password';
 import { InputText } from 'primereact/inputtext';
 
+
 const Login = () => {
   const [email, setemail] = useState("");
   const [password, setpwd] = useState("");
+  const [errorEmail, setErrorEmail] = useState(false);
+  const [errorPassword, setErrorPassword] = useState(false);
+  const [evaluacion, setEvaluacion] = useState('');
+
+  const validarEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+
+  const validarPassword = (password) => {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@$!%*?&.]{8,}$/;
+    return regex.test(password);
+  };
+
+  const cambioCampoEmail = (i) => {
+    const valor = i.target.value;
+    setemail(valor);
+    setErrorEmail(() => !validarEmail(valor) && valor.length > 0);
+  };
+
+  const cambioCampoPassword = (i) => {
+    const valor = i.target.value;
+    setpwd(valor);
+    // setErrorPassword(() => !validarPassword(valor) && valor.length > 0)
+  }
+
+  const validarCampos = (e) => {
+    e.preventDefault();
+    if (errorEmail || errorPassword || !email || !password) {
+      setEvaluacion("Por favor, corrige los errores antes de enviar.");
+    }
+    else {
+      setEvaluacion("");
+      validar();
+    }
+  }
 
   const validar = () => {
     LoginServices.ingreso({
@@ -19,14 +56,18 @@ const Login = () => {
         const usuario = response.data[0]; // Accede al primer usuario en los resultados
         sessionStorage.setItem('id', JSON.stringify(usuario.id_Usuario)); // Convierte el id a una cadena antes de guardarlo
         sessionStorage.setItem('data', JSON.stringify(usuario)); // Convierte el objeto usuario a una cadena antes de guardarlo
-        window.location.href = '/Usuario';
+        window.location.href='/PrestamoN';
       }
 
     }).catch(error => { //Excepciones
       if (error.status === 401) {
-
+        setErrorEmail(true);
+        setErrorPassword(true);
+        setEvaluacion("Correo o contraseña incorrecta.");
       } else if (error.status === 402) {
-
+        setErrorEmail(true);
+        setErrorPassword(true);
+        setEvaluacion("Correo o contraseña incorrecta.");
       }
     })
   }
@@ -45,33 +86,31 @@ const Login = () => {
             <label>Email</label>
             <InputText
               value={email}
-              onChange={(e) => setemail(e.target.value)}
-              className="inputsFormulario"
+              onChange={(e) => cambioCampoEmail(e)}
+              className={`inputsFormulario ${errorEmail ? "error-border" : ""}`}
               placeholder="Ingresa tu email"
             />
             <label>Password</label>
             <Password
               value={password}
-              onChange={(e) => setpwd(e.target.value)}
+              onChange={(e) => cambioCampoPassword(e)}
               feedback={false}
-              className="inputsPwd"
+              className={`inputsPwd ${(errorPassword) ? "error-border": ""}`}
               placeholder="*********"
-              toggleMask 
-              />
+              toggleMask
+            />
             <div>
               <input
                 className="checkboxFormulario"
                 type="checkbox"
-                name=""
-                id=""
               />
               <span className="span-recordatorio">Recordarme por 30 días</span>
-
             </div>
 
-            <button className="boton-inicio-sesion" onClick={validar}>Iniciar sesion</button>
-
+            <button className="boton-inicio-sesion" onClick={(e) => validarCampos(e)}>Iniciar sesion</button>
           </form>
+          <br></br>
+          {evaluacion && <div className="mensaje-evaluacion">{evaluacion}</div>}
         </div>
 
         <div className="container-uabcRectoria">
